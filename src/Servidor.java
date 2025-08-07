7b747b7ac675d8d4b37831a635a8cdeaa6145989
oscar emanuel diaz castañeda <oscardc.oedc@gmail.com>
2025-08-06 17:23:13 -0700

    cambio 1

--- src/Servidor.java
+++ src/Servidor.java
@@ -2,10 +2,9 @@ public class Servidor {
     public static void main(String[] args) {
         System.out.println("HOLA");
         int pruebaCommit = 1;
-        if (pruebaCommit == 1) {
-            pruebaCommit = 0;
-            
-        }
+        int oscar;
+        
+        System.out.println("hola");
     }
 
 }
